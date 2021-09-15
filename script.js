22397a3fa4f1aa8dde402e128e9444eaed630aed
cd <coolezprogramming@gmail.com>
2021-09-15 17:26:43 -0400

    added slider and button click for oeprators

--- script.js
+++ script.js
@@ -14,4 +14,18 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, num1, num2) {}
+function operate(operator, num1, num2) {
+    console.log(operator);
+}
+
+const operators = document.querySelectorAll(".operator");
+
+operators.forEach((operator) =>
+    operator.addEventListener("click", () => {
+        operate(operator.id);
+        {
+            //TODO
+            //operator.style.transform = "scale(0.8)";
+        }
+    })
+);
