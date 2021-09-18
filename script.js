cf60ae26c7e0a10606f2c4c67f4405eeb71cb67f
cd <coolezprogramming@gmail.com>
2021-09-17 23:19:06 -0400

    added screen and ability to clear text

--- script.js
+++ script.js
@@ -18,14 +18,32 @@ function operate(operator, num1, num2) {
     console.log(operator);
 }
 
-const operators = document.querySelectorAll(".operator");
+const operators = document.querySelectorAll(".top, .operator");
+
+const equation = document.querySelector("#equation");
 
 operators.forEach((operator) =>
     operator.addEventListener("click", () => {
         operate(operator.id);
         {
-            //TODO
-            //operator.style.transform = "scale(0.8)";
+            if (
+                operator.className === "operator" ||
+                operator.className === "number"
+            ) {
+                equation.textContent += " " + operator.textContent;
+
+                //TODO create function to take all content
+            } else if (operator.id === "clear") {
+                equation.textContent = "";
+            }
         }
     })
 );
+
+const numbers = document.querySelectorAll(".number");
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        equation.textContent += number.textContent;
+    });
+});
