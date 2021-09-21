0613366c12860413b413c0bbd0380d22ac0fec43
cd <coolezprogramming@gmail.com>
2021-09-20 23:15:53 -0400

    numbers go into variable when equal is entered

--- script.js
+++ script.js
@@ -14,22 +14,34 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, num1, num2) {}
+function operate(operators, nums) {
+    console.log(1);
+}
 
 const buttons = document.querySelectorAll(".button, .top");
 
 const equation = document.querySelector("#equation");
 let equationLength = equation.clientWidth;
 
+let operators = [];
+let nums = [];
+
+
 buttons.forEach((button) =>
     button.addEventListener("click", () => {
-        console.log(equation.textContent, button.textContent);
         if (button.className === "button operator") {
+            nums.push(parseInt(equation.textContent.split(" ").slice(-1).join(", ")));
+            operators.push(button.textContent);
+            if (button.id === "equal") {
+                operate(operators, nums);
+            }
             equation.textContent += " " + button.textContent + " ";
         } else if (button.id === "clear") {
+            operators = [];
+            nums = [];
             equation.textContent = "";
         } else if (button.className === "button number") {
             equation.textContent += button.textContent;
-        } 
+        }
     })
 );
