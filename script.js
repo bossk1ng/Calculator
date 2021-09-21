afd0d9fd7fc5b39f41863a20b2fc27c8f0116c00
ok <coolezprogramming@gmail.com>
2021-09-21 11:02:39 -0400

    added basic calculator functions

--- script.js
+++ script.js
@@ -14,10 +14,29 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operators, nums) {
-    console.log(1);
-}
 
+const value = document.getElementById("value");
+
+function operate(operators, nums) {
+    let valueText = 0;
+
+    console.log(nums.reduce((a, b) => add(a, b)))
+
+    operators.forEach((operator) => {
+        if (operator === "+") {
+            valueText = nums.reduce((a, b) => add(a, b))
+        } else if (operator === "-") {
+            valueText = nums.reduce((a, b) => subtract(a, b))
+        } else if (operator === "×") {
+            valueText = nums.reduce((a, b) => multiply(a, b))
+        } else {
+            valueText = nums.reduce((a, b) => divide(a, b))
+        }
+    });
+    value.textContent = Math.round(valueText * 100) / 100;
+    nums.shift()
+    nums.shift()
+}
 const buttons = document.querySelectorAll(".button, .top");
 
 const equation = document.querySelector("#equation");
@@ -26,19 +45,24 @@ let equationLength = equation.clientWidth;
 let operators = [];
 let nums = [];
 
-
 buttons.forEach((button) =>
     button.addEventListener("click", () => {
         if (button.className === "button operator") {
-            nums.push(parseInt(equation.textContent.split(" ").slice(-1).join(", ")));
-            operators.push(button.textContent);
+            nums.push(
+                parseFloat(equation.textContent.split(" ").slice(-1).join(", "))
+            );
             if (button.id === "equal") {
                 operate(operators, nums);
+                equation.textContent = value.textContent;
+                return;
             }
+            operators.push(button.textContent);
             equation.textContent += " " + button.textContent + " ";
         } else if (button.id === "clear") {
             operators = [];
             nums = [];
+            valueText = ""
+            value.textContent = ""
             equation.textContent = "";
         } else if (button.className === "button number") {
             equation.textContent += button.textContent;
